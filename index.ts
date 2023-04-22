a97e0cfce9286e0ca57a080cfcfbd75a937a43a3
Gordon Sun <gordon@typeface.ai>
2023-04-22 12:53:22 -0700

    Init commit that working on Mac

---
+++ index.ts
@@ -0,0 +1,41 @@
+import * as azdev from "azure-devops-node-api";
+import * as dotenv from 'dotenv';
+import * as GitApi from "azure-devops-node-api/GitApi";
+import * as fs from 'fs';
+import path from 'path';
+import * as shelljs from 'shelljs';
+
+dotenv.config();
+
+const reposFolder = process.env.reposFolder || '~';
+const azGitUrlBase = process.env.azGitUrlBase;
+const azOrganizationUrl = process.env.azOrganizationUrl || '';
+const azPersonalAccessToken = process.env.azPersonalAccessToken || '';
+
+const authHandler = azdev.getPersonalAccessTokenHandler(azPersonalAccessToken); 
+const connection = new azdev.WebApi(azOrganizationUrl, authHandler);    
+
+let main = async () => {
+    const git: GitApi.GitApi = await connection.getGitApi();
+    const repos = await git.getRepositories();
+    for (const repo of repos) {
+        const localPath = `${reposFolder}/${repo.name}`
+        console.log(`Checking if folder ${localPath} exists`)
+        const exists = fs.existsSync(localPath)
+        if (exists) {
+            console.log(`Folder exists`)
+        }
+        else {
+            console.log(`Folder does not exists`)
+            const githttps = azGitUrlBase + '/' + encodeURIComponent(repo.name ?? '');
+            const localPathNormalized = path.normalize(localPath);
+            const cloneCmd = `git clone ${githttps} ${localPathNormalized}`
+            console.log(cloneCmd)
+            shelljs.exec(cloneCmd)
+        }
+        shelljs.cd(localPath)
+        shelljs.exec('git pull --all')
+    }
+}
+
+main()
